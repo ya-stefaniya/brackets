e58d14a59dae6230842269b259972c99acba6057
Stefaniya Yanovich <stefanishy@gmail.com>
2020-10-16 14:39:15 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let newBC = bracketsConfig.map(item => item.join('')); 
+    let lengthOld = str.length;
+    
+      while(str.length > 0){
+        for (let i=0; i < newBC.length; i++){
+      str = str.replace(newBC[i], '');
+        }
+        if (str.length === lengthOld)
+        return false;
+        else if(str.length === 0)
+        return true;
+        else
+        lengthOld = str.length;
+      };
+       
+       
+        
+    }
+
